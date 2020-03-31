308acaebbbcc208380f82d7fde276d4b8f378907
bolshed <bolshed@protonmail.com>
2020-03-31 18:59:47 +0200

    netlify-setup-almost-done

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventConfig) {
     eleventConfig.addPassthroughCopy('img');
     eleventConfig.addPassthroughCopy('js');
     eleventConfig.addPassthroughCopy('vendor');
+    eleventConfig.addPassthroughCopy('admin');
     eleventConfig.addFilter('dump', obj => {
         return util.inspect(obj);
     })
