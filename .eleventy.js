62775a2fba2c2820b7b2955da2f557ab54801e3d
bolshed <bolshed@protonmail.com>
2020-04-04 18:52:00 +0200

    algolia-search-almost-done

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,41 @@
 const util = require('util');
-module.exports = function (eleventConfig) {
-    eleventConfig.addPassthroughCopy('css');
-    eleventConfig.addPassthroughCopy('img');
-    eleventConfig.addPassthroughCopy('js');
-    eleventConfig.addPassthroughCopy('vendor');
-    eleventConfig.addPassthroughCopy('admin');
-    eleventConfig.addFilter('dump', obj => {
-        return util.inspect(obj);
+const algoliasearch = require('algoliasearch');
+const searchClient = algoliasearch('JL7BROHM5Z', 'd36c790225de9ec79cefd7c0e728619e');
+const algoliaIndex = searchClient.initIndex('blog');
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('css');
+  eleventyConfig.addPassthroughCopy('img');
+  eleventyConfig.addPassthroughCopy('js');
+  eleventyConfig.addPassthroughCopy('vendor');
+  eleventyConfig.addPassthroughCopy('admin');
+
+  eleventyConfig.addFilter('dump', obj => util.inspect(obj));
+
+  eleventyConfig.addFilter('formatDate', value => {
+    const ISOcode = 'en-GB';
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const d = new Date(value);
+    return `${d.toLocaleDateString(ISOcode, options)}`;
+  })
+
+  eleventyConfig.addFilter('sanitize', value => value.split(' ').join('-').toLowerCase())
+  
+
+  eleventyConfig.addCollection('algolia', collection => {
+    const index = collection.getAll().filter(item => {
+      let extension = item.inputPath.split('.').pop();
+      return extension === 'md';
+    }).map(item => {
+      return {
+        objectID: item.data.page.url,
+        title: item.data.title,
+        author: item.data.author,
+        description: item.data.description,
+        url: item.data.page.url,
+        content: JSON.stringify(item.template.frontMatter.content)
+      }
     });
-    eleventConfig.addFilter('formatDate', date => {
-        const ISOcode = 'en-GB';
-        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-        const d = new Date(date);
-        return `${d.toLocaleDateString(ISOcode, options)}`;
-    })
-    eleventConfig.addFilter('sanitize', value => value.split(' ').join('-').toLowerCase())
-}
+    return algoliaIndex.saveObjects(index);
+  })
+};
--- _site/js/clean-blog.js
+++ _site/js/clean-blog.js
@@ -1,6 +1,49 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const searchClient = algoliasearch('JL7BROHM5Z', 'd36c790225de9ec79cefd7c0e728619e');
+    
+  const search = instantsearch({
+    indexName: 'blog',
+    searchClient,
+    searchFunction: function(helper) {
+      var searchResults = $('#search_results');
+      var hits = $('#hits');
+      if (helper.state.query === '') {
+        // empty query string -> hide the search results & abort the search
+        hits.hide();
+        searchResults.hide();
+        return;
+      }
+      // perform the regular search & display the search results
+      helper.search();
+      searchResults.show();
+      hits.show();
+    }
+  });
+
+  search.addWidgets([
+    instantsearch.widgets.searchBox({
+      container: '#searchbox',
+    }),
+
+    instantsearch.widgets.hits({
+      container: '#hits',
+      templates: {
+        item: `
+          <p>
+            <a href="{{url}}">{{ title }}</a>
+          </p>
+          
+        `,
+      }
+    })
+  ]);
+
+  search.start();
+});
+
+
 (function($) {
   "use strict"; // Start of use strict
-
   // Floating label headings for the contact form
   $("body").on("input propertychange", ".floating-label-form-group", function(e) {
     $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
@@ -39,3 +82,45 @@
   }
 
 })(jQuery); // End of use strict
+
+// (function($) {
+//   "use strict"; // Start of use strict
+
+//   // Floating label headings for the contact form
+//   $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+//     $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+//   }).on("focus", ".floating-label-form-group", function() {
+//     $(this).addClass("floating-label-form-group-with-focus");
+//   }).on("blur", ".floating-label-form-group", function() {
+//     $(this).removeClass("floating-label-form-group-with-focus");
+//   });
+
+//   // Show the navbar when the page is scrolled up
+//   var MQL = 992;
+
+//   //primary navigation slide-in effect
+//   if ($(window).width() > MQL) {
+//     var headerHeight = $('#mainNav').height();
+//     $(window).on('scroll', {
+//         previousTop: 0
+//       },
+//       function() {
+//         var currentTop = $(window).scrollTop();
+//         //check if user is scrolling up
+//         if (currentTop < this.previousTop) {
+//           //if scrolling up...
+//           if (currentTop > 0 && $('#mainNav').hasClass('is-fixed')) {
+//             $('#mainNav').addClass('is-visible');
+//           } else {
+//             $('#mainNav').removeClass('is-visible is-fixed');
+//           }
+//         } else if (currentTop > this.previousTop) {
+//           //if scrolling down...
+//           $('#mainNav').removeClass('is-visible');
+//           if (currentTop > headerHeight && !$('#mainNav').hasClass('is-fixed')) $('#mainNav').addClass('is-fixed');
+//         }
+//         this.previousTop = currentTop;
+//       });
+//   }
+
+// })(jQuery); // End of use strict
